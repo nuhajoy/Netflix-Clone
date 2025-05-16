59b7bd17c7e60e7a12838070444511d2553e8f77
nuhajoy <nuhaminemnebered@gmail.com>
2025-05-16 11:01:29 +0300

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home/Home'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello</h1>
-        
-    </>
+    <div className='App'>
+          <Home />
+    </div>
   )
 }
 
---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import React from 'react-dom'
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer";
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home
