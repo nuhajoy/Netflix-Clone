3afeab770026dff5c0b3056b0a640d6d0b72968c
nuhajoy <nuhaminemnebered@gmail.com>
2025-05-19 13:14:16 +0300

    row

---
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import requests from '../../utils/requests'
+import axios from '../../utils/axios';
+import './banner.css'
+
+function Banner() {
+  const truncate = (str, length) => {
+    return str?.length > length ? str.substring(0, length) + "..." : str;
+  };
+
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, []);
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button play">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+      <div className="banner_fadeBottom"></div>
+    </div>
+  );
+}
+
+export default Banner;
